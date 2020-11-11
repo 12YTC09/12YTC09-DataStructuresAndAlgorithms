1886c156e6210b954403a55af5a131d022e84b29
12YTC09 <p0361533@o365.fcu.edu.tw>
2020-11-11 14:33:42 +0800

    ch01

---
+++ CH01/classC.cpp
@@ -0,0 +1,37 @@
+#include "classC.H"
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+
+   C object1("object1",100,200);
+
+   object1.memberFunction1();
+
+   object1.memberFunction2(123);
+
+   C object2("object2");
+
+
+
+   C object3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
