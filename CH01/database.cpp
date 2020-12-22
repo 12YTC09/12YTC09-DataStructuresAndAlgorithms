8f221c475b18ab93af0fc90e95371cb60ad8f674
12YTC09 <p0361533@o365.fcu.edu.tw>
2020-12-22 14:54:56 +0800

    Ch01

--- CH01/database.cpp
+++ CH01/database.cpp
@@ -26,6 +26,76 @@ template<class T>
 void Database<T>::modify(const T& d){
     T tmp;
     database.open(fName,ios::in|ios::out|ios::binary);
-    while()
+    while(!database.eof){
+        tmp.readFromFile(database);
+        if(tmp == d){   //overloaded ==
+            cin >> tmp; // overloaded >>
+            database.seekp(-d.size(),ios::cur);
+            tmp.writeToFile(database);
+            database.close();
+            return;
+        }
+    }
+    database.close();
+    cout << "The record to be modified is not in the database\n";
+}
+
+template<class T>
+bool Database<T>::find(const T &d){
+    T tmp;
+    database.open(fName,ios::in|ios::binary);
+    while(true){
+        tmp.readFromFile(database);
+        if(database.eof())
+            break;
+        out << tmp <<endl;  //overloaded<<
+    }
+    database.close();
+    return out;
+}
+
+template<class T>
+void Database<T>::run(){
+    cout << "File name: ";
+    cin >> fName;
+    char option[5];
+    T rec;
+    cout << "1. Add 2. Find 3. Modify a record; 4. Exit \n" ;
+    cin.getline(option,4);  //get '\n';
+    while(cin.getline(option,4)){
+        if (*option == '1'){
+            cin >> rec; //
+            add(rec);
+        }
+        else if (*option == '2'){
+            rec.readkey();
+            cout << "The record is ";
+            if (find(rec) == false)
+                cout << "not ";
+            cout << "in the database \n";
+        }
+        else if(*option == '3'){
+            rec.readkey();
+            modify(rec);
+        }
+        else if(*option != '4')
+            cout << "Wrong option\n";
+        else return;
+        cout << *this; 
+        cout << "Enter an option: ";       
+    }
 
 }
+
+int main(){
+    Database<Personal>().run();
+    //
+    //Database<Student>().run();
+    return 0;
+
+
+
+}
+
+
+
